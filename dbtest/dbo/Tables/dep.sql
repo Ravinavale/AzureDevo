f22c870d9661b809bd1ed5ebfbd5f359507c2916
Admin <Admin@AROKIAIT-146>
2022-04-01 17:08:27 +0530

    db changes

--- dbtest/dbo/Tables/dep.sql
+++ dbtest/dbo/Tables/dep.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[dep] (
     [department_id]   BIGINT       IDENTITY (1, 1) NOT NULL,
-    [department_name] VARCHAR (1000) NULL,
+    [department_name] VARCHAR (50) NULL,
     CONSTRAINT [PK_dep] PRIMARY KEY CLUSTERED ([department_id] ASC)
 );
 

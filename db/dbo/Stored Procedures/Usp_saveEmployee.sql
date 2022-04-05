c7026e01c4d747d7ba036d2bf90fcaf18d59b636
Admin <Admin@AROKIAIT-146>
2022-04-05 19:27:06 +0530

    delete

--- db/dbo/Stored Procedures/Usp_saveEmployee.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE dbo.Usp_saveEmployee
-@employee varchar(50)
-AS
-begin
-	insert into  Employee
-	select @employee
-end
--- db/dbo/Tables/Employee.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee] (
-    [id]       INT          IDENTITY (1, 1) NOT NULL,
-    [Employee] VARCHAR (15) NULL,
-    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([id] ASC)
-);
-

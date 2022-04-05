6aa3c9bf051474c4befbc82c2dda3e39b181baa5
Admin <Admin@AROKIAIT-146>
2022-04-05 18:59:11 +0530

    demo for devlopers

---
+++ db/dbo/Tables/emp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[emp] (
+    [emp_id]        BIGINT       IDENTITY (1, 1) NOT NULL,
+    [Employee_name] VARCHAR (50) NULL,
+    [email]         VARCHAR (50) NULL,
+    [mobile]        VARCHAR (50) NULL,
+    [department_id] BIGINT       NULL,
+    [salary]        BIGINT       NULL,
+    CONSTRAINT [PK_emp] PRIMARY KEY CLUSTERED ([emp_id] ASC)
+);
+

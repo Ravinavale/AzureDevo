a7291bd14925c58055376ae3648a40c6cccc1c39
Admin <Admin@AROKIAIT-146>
2022-06-03 14:26:26 +0530

    approved

--- AzureDev0/Index.aspx.cs
+++ AzureDev0/Index.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,41 @@ namespace AzureDev0
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Generatereport(DataTable dtCodeUsage, string ReportPath, string DataTable, string exportType)
+        {
+            try
+            {
+                ReportViewer rdlcQrCodeUploadListReport = new ReportViewer();
+                rdlcQrCodeUploadListReport.LocalReport.DataSources.Clear();
+                rdlcQrCodeUploadListReport.SizeToReportContent = true;
+                rdlcQrCodeUploadListReport.LocalReport.ReportPath = Server.MapPath("~/" + ReportPath);
+                rdlcQrCodeUploadListReport.LocalReport.EnableHyperlinks = true;
+                rdlcQrCodeUploadListReport.LocalReport.Refresh();
+               
+               
+                if (exportType == "Excel")
+                {
+                    Warning[] warnings;
+                    string[] streamIds;
+                    string mimeType = string.Empty;
+                    string encoding = string.Empty;
+                    string extension = string.Empty;
+                    string fileName = "Material Wise Report" + "_" + System.DateTime.Now.ToString("ddMMyyyy_hhmm");
+                    byte[] bytes = rdlcQrCodeUploadListReport.LocalReport.Render(exportType, null, out mimeType, out encoding, out extension, out streamIds, out warnings);
+                    Response.Buffer = true;
+                    Response.Clear();
+                    Response.ContentType = mimeType;
+                    Response.AddHeader("content-disposition", "attachment; filename=" + fileName + "." + extension);
+                    Response.BinaryWrite(bytes); // create the file
+                    Response.Flush(); // send it to the client to download
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
